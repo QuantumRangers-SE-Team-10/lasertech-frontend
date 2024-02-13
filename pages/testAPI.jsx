be33c8767a95efc8e4395d9794d6f7f3f2ef2889
shawnericksen <shawnericksen.github@protonmail.com>
2024-02-13 13:25:54 -0600

    fix api functions calls

--- pages/testAPI.jsx
+++ pages/testAPI.jsx
@@ -1,10 +1,8 @@
 import { useEffect, useState } from 'react';
 import getPlayers from '../api/player'
 
-export default TestAPI;
-
 function TestAPI() {
-  const [value, setValue] = useState('');
+  const [players, setValue] = useState('');
 
   useEffect(() => {
     async function getValue() {
@@ -15,10 +13,20 @@ function TestAPI() {
     getValue();
   }, []);
 
+  useEffect(() => {
+    console.log(players);
+  }, [players]);
+
+  let player1Codename = '';
+
+  if (players != '') {
+    player1Codename = players.data[0].codename;
+  }
+
   return (
     <>
       <p>
-        value is {value}
+        Player 1 is {player1Codename}
       </p>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -26,3 +34,5 @@ function TestAPI() {
     </>
   )
 }
+
+export default TestAPI;
