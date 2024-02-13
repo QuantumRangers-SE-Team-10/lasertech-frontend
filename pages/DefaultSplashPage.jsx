1124fdb8d118bac52f2e603e672f8ca0ab66d697
Shawn-Wheeler <swheeler@uark.edu>
2024-02-13 14:15:23 -0600

    Splash page started

--- pages/DefaultSplashPage.jsx
+++ pages/DefaultSplashPage.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from '../src/assets/react.svg'
-import viteLogo from '../public/vite.svg'
+import gameLogo from '../src/assets/logo.jpg'
 
-export default DefaultSlashPage;
+export default DefaultSplashPage;
 
-function DefaultSlashPage() {
-  const [count, setCount] = useState(0)
+function DefaultSplashPage() {
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <img src={gameLogo} className="logo" alt="Game logo" />
+      </div>
+      <div>
+        <a href="/onboarding"> {/*what is onboarding page called, link to that*/}
+          <button>Start</button> {/*add css stuff*/}
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
