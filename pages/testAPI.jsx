95bb5e0c76ba92ee7262c73bf01ceb390e3dec43
shawnericksen <shawnericksen.github@protonmail.com>
2024-02-13 12:32:28 -0600

    wip frontend api

---
+++ pages/testAPI.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import getPlayers from '../api/player'
+
+export default TestAPI;
+
+function TestAPI() {
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    async function getValue() {
+      const response = await getPlayers();
+      setValue(response);
+    }
+    
+    getValue();
+  }, []);
+
+  return (
+    <>
+      <p>
+        value is {value}
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </>
+  )
+}
