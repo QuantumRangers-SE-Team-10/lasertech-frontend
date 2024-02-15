5eab8fced8cfc8646462182fe2d4d43f41607ea3
Shawn-Wheeler <swheeler@uark.edu>
2024-02-14 20:03:18 -0600

    Splash page now has countdown timer and redirects to /onboarding in 3 seconds

--- pages/DefaultSplashPage.jsx
+++ pages/DefaultSplashPage.jsx
@@ -1,18 +1,28 @@
 import gameLogo from '../src/assets/logo.jpg'
+import React, { useState, useRef, useEffect } from "react";
+import '../src/css/Splash.css'
 
 export default DefaultSplashPage;
 
 function DefaultSplashPage() {
+
+  let timeLeft = 3;
+  setInterval(() => {
+    document.getElementById("countdown").innerHTML = timeLeft;
+    timeLeft -= 1;
+    if(timeLeft < 0) {
+      timeLeft = 0;
+    }
+  }, 1000);
 
   return (
     <>
       <div>
         <img src={gameLogo} className="logo" alt="Game logo" />
       </div>
+      <div id="countdown" className="countdown"></div>
       <div>
-        <a href="/onboarding"> {/*what is onboarding page called, link to that*/}
-          <button>Start</button> {/*add css stuff*/}
-        </a>
+        <meta http-equiv="refresh" content="3;url=/onboarding" />
       </div>
     </>
   )
