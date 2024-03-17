41b97136fd53d75fd029d5192292404e0101835c
shawnericksen <shawnericksen.github@protonmail.com>
2024-03-17 14:09:54 -0500

    add udp server

---
+++ server.cjs
@@ -0,0 +1,101 @@
+/* eslint-disable no-undef */
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST']
+  }
+});
+
+const dgram = require('dgram');
+const PORT = 7501;
+const HOST = '0.0.0.0';
+const server = dgram.createSocket('udp4');
+
+server.on('listening', () => {
+  const address = server.address();
+  console.log(`UDP Server listening on ${address.address}:${address.port}`);
+});
+
+server.on('message', (message, remote) => {
+  console.log(`Received message from ${remote.address}:${remote.port}: ${message}`);
+
+  // Broadcast the message to all connected clients
+  io.emit('udp-message', message.toString());
+
+  // Send a response to the client
+  // const response = Buffer.from('Message received by server');
+  // server.send(response, 0, response.length, 7500, '127.0.0.1', (err) => {
+  //   if (err) {
+  //     console.error('Failed to send response:', err);
+  //   } else {
+  //     console.log('Response sent to client');
+  //   }
+  // });
+});
+
+server.bind(PORT, HOST);
+
+io.on('connection', (socket) => {
+  console.log('A client connected');
+
+  function sendUDPMessage(message) {
+    const buffer = Buffer.from(message);
+    server.send(buffer, 0, buffer.length, 7500, '127.0.0.1', (err) => {
+      if (err) {
+        console.error('Failed to send message:', err);
+      } else {
+        console.log('Message sent');
+      }
+    });
+  }
+
+  function sendKeyUDPMessage(message) {
+    const buffer = Buffer.from(message);
+    Array.from({ length: 3 }, () => {
+      server.send(buffer, 0, buffer.length, 7500, '127.0.0.1', (err) => {
+        if (err) {
+          console.error('Failed to send message:', err);
+        } else {
+          console.log('Message sent');
+        }
+      });
+    });
+  }
+
+  socket.on('udp-send', (message) => {
+    sendUDPMessage(message);
+  });
+
+  socket.on('equipment-init', (message) => {
+    sendKeyUDPMessage(message);
+  });
+
+  socket.on('game-start', () => {
+    sendKeyUDPMessage('202');
+  });
+
+  socket.on('game-end', () => {
+    sendKeyUDPMessage('221');
+  });
+
+  socket.on('red-base-scored', () => {
+    sendUDPMessage('53');
+  });
+
+  socket.on('green-base-scored', () => {
+    sendUDPMessage('43');
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A client disconnected');
+  });
+});
+
+
+const PORT_HTTP = 3000;
+http.listen(PORT_HTTP, () => {
+  console.log(`HTTP Server listening on port ${PORT_HTTP}`);
+});
